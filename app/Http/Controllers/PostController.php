12d1407d6511c68abc652d151bbc8110b816ff08
KohheiAdachi <kohei.adachi805@mail.kyutech.jp>
2021-12-03 02:19:11 +0900

    task-1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,11 +10,6 @@ class PostController extends Controller
 {
     public function store(StorePostRequest $request)
     {
-        $request->validate(
-            // ... TASK: write validation here so that "title" field
-            //           would be required and unique in the "posts" DB table
-        );
-
         // Saving the post
         Post::create(['title' => $request->title]);
 
