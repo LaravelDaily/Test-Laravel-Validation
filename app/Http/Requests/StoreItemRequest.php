9d984301afa8b94abdb049f846cf728300dfa6d2
Peter Bessenyei <peter.bessenyei@gmail.com>
2021-12-02 11:11:38 +0100

    fix tests

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -4,10 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreBuildingRequest extends FormRequest
+class StoreItemRequest extends FormRequest
 {
-    protected $redirectRoute = 'buildings.create';
-
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -26,14 +24,8 @@ class StoreBuildingRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
-        ];
-    }
-
-    public function messages()
-    {
-        return [
-            'name.required' => 'Please enter the name'
+            'name' => 'required',
+            'description' => 'required'
         ];
     }
 }
