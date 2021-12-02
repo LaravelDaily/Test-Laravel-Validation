00940cb435ec80694645d273bcfc1d2769f479b4
Recep Doku <recep@revy.com.tr>
2021-12-02 15:01:12 +0300

    OK

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,6 +9,8 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
         $request->validate([
+            'profile.name' => 'required',
+            'profile.email' => 'required',
             // TASK: imagine that in the Blade the fields are
             // <input name="profile[name]" ... />
             // <input name="profile[email]" ... />
