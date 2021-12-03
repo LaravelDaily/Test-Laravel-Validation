70b85d306875aed634fd9aecf60cfe8ca55701ca
alessandrofuda <alessandro.fuda@gmail.com>
2021-12-03 12:19:09 +0100

    tests solved! Bye :)

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,6 +3,11 @@
 {{-- TASK: add the validation errors here - with whatever HTML structure you want --}}
 {{-- in case of title/description empty, visitor should see --}}
 {{-- "The name field is required." and "The description field is required." --}}
+@if($errors->any())
+    @foreach ($errors->all() as $error)
+        <p class="alert">{{$error}}</p>
+    @endforeach
+@endif
 
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
