e0e0afcea58d53e2ac804bc8e5552d4d9cfa58c3
Hasan97 <alasary97@gmail.com>
2021-12-03 11:06:23 +0700

    main

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,6 +13,8 @@ class ProfileController extends Controller
             // <input name="profile[name]" ... />
             // <input name="profile[email]" ... />
             // Write validation rules, so both name and email are required
+            'profile'   => 'required|array',
+            'profile.*' => 'required',
         ]);
 
         auth()->user()->update($request->profile ?? []);
