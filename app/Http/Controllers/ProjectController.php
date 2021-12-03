d1725156c46ee9bdbdc6f24d78e4d7db4e2c8bdb
jwmcross <59067337+jwmcross@users.noreply.github.com>
2021-12-03 11:28:50 +0000

    Task 3. Showing Validation Errors

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
+            'name' => 'required' ,
             'description' => 'required'
         ]);
 
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,8 +1,18 @@
+
 {{-- Form without any design --}}
 
 {{-- TASK: add the validation errors here - with whatever HTML structure you want --}}
 {{-- in case of title/description empty, visitor should see --}}
 {{-- "The name field is required." and "The description field is required." --}}
+
+@error('name')
+<p>{{ $errors->first('name') }}</p>
+@enderror
+
+@error('description')
+<p>{{ $errors->first('description') }}</p>
+@enderror
+
 
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
