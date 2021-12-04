409bf0856f827b455d11862d5ce838c398c03549
Tomas Šimkevičius <tomas.simkevicius@gmail.com>
2021-12-04 23:53:27 +0200

    Pass all validation tests.

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -7,9 +7,6 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
-    // TASK: generate that StoreItemRequest class
-    //   with authorization returning TRUE
-    //   and validation rules: name is required, and description is required
     public function store(StoreItemRequest $request)
     {
         Item::create($request->validated());
