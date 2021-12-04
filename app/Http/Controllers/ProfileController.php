813de86c9d02242e9b173cef55f89f3c2387981f
Ali Macbook <alila3883@gmail.com>
2021-12-04 12:03:23 +0300

    Complete laravel validation test

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,10 +9,10 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
         $request->validate([
-            // TASK: imagine that in the Blade the fields are
-            // <input name="profile[name]" ... />
-            // <input name="profile[email]" ... />
-            // Write validation rules, so both name and email are required
+            'profile' => [
+                'name' => ['required'],
+                'email' => ['required'],
+            ]
         ]);
 
         auth()->user()->update($request->profile ?? []);
