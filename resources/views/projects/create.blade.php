c3f1f33588b792d8364d60c0ec4a2c41b20ce779
tauseed zaman <tauseedzamanofsorana@gmail.com>
2021-12-05 17:03:57 +0500

    test 3 pass

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,16 +3,21 @@
 {{-- TASK: add the validation errors here - with whatever HTML structure you want --}}
 {{-- in case of title/description empty, visitor should see --}}
 {{-- "The name field is required." and "The description field is required." --}}
-
+<ul class="list-unstyled">
+    <li>@error('title')@enderror</li>
+    <li></li>
+</ul>
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
     Title:
     <br />
     <input type="text" name="title" />
+    @error('name')<span class="text-danger">{{  $message }}</span>@enderror
     <br /><br />
     Description:
     <br />
     <input type="text" name="description" />
+    @error('description')<span class="text-danger">{{ $message }}</span>@enderror
     <br /><br />
     <button type="submit">Save</button>
 </form>
--- tests/Feature/ValidationTest.php
+++ tests/Feature/ValidationTest.php
@@ -43,20 +43,20 @@ class ValidationTest extends TestCase
         $response->assertStatus(200);
    }
 
-//     public function test_validation_errors_shown_in_blade()
-//     {
-//         $response = $this->followingRedirects()->post('projects');
-//         $response->assertStatus(200);
-//         $response->assertSee('The name field is required.');
-//         $response->assertSee('The description field is required.');
-//     }
+    public function test_validation_errors_shown_in_blade()
+    {
+        $response = $this->followingRedirects()->post('projects');
+        $response->assertStatus(200);
+        $response->assertSee('The name field is required.');
+        $response->assertSee('The description field is required.');
+    }
 
-//     public function test_validation_specific_error_shown_in_blade()
-//     {
-//         $response = $this->followingRedirects()->post('products');
-//         $response->assertStatus(200);
-//         $response->assertSee('The name field is required.');
-//     }
+    // public function test_validation_specific_error_shown_in_blade()
+    // {
+    //     $response = $this->followingRedirects()->post('products');
+    //     $response->assertStatus(200);
+    //     $response->assertSee('The name field is required.');
+    // }
 
 //     public function test_old_value_stays_in_form_after_validation_error()
 //     {
