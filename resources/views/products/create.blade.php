2518284cca8cf09f97e43f046bfc182a8880c543
Mateus Santana <mateusssantana@gmail.com>
2021-12-06 12:27:54 -0300

    Task 4

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,6 +3,9 @@
     Name:
     <br />
     <input type="text" name="name" />
+    @error('name')
+        <small class="alert alert-danger">{{ $message }}</small>
+    @enderror
     <br />
     {{-- TASK: show the validation error for the specific "name" field --}}
     {{-- using one Blade directive: pseudo-code below --}}
