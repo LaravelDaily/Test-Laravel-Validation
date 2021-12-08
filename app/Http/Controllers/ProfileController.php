af7d2b24f7b6b7fbd95752ad71f43a47601cb2b2
Guus Frenken <g.frenken@qteco.nl>
2021-12-08 10:02:36 +0100

    All tasks completed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,10 +9,8 @@ class ProfileController extends Controller
     public function update(Request $request)
     {
         $request->validate([
-            // TASK: imagine that in the Blade the fields are
-            // <input name="profile[name]" ... />
-            // <input name="profile[email]" ... />
-            // Write validation rules, so both name and email are required
+            'profile.name' => 'required',
+            'profile.email' => 'required',
         ]);
 
         auth()->user()->update($request->profile ?? []);
