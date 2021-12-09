1ef7d5944719fdaada62e806ed920791921037b5
Leo Nuneza <leiyu876@yahoo.com>
2021-12-09 15:09:24 +0800

    Done

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,3 +1,12 @@
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form method="POST" action="{{ route('products.store') }}">
     @csrf
     Name:
