67aad3da15c10a3abc4fe8ad814cee4729d4cc02
BeshoyFD <bishoy.eagle@yahoo.com>
2021-12-10 06:03:29 +0200

    tasks completed

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -6,9 +6,9 @@
     <br />
     {{-- TASK: show the validation error for the specific "name" field --}}
     {{-- using one Blade directive: pseudo-code below --}}
-    {{-- @directive --}}
-    {{-- {{ $message }} --}}
-    {{-- @endDirective --}}
+    @error('name')
+    <ul><li>{{ $message }}</li></ul>
+    @enderror
     <br /><br />
     <button type="submit">Save</button>
 </form>
