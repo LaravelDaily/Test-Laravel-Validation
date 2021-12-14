6b0136647c51aa845e50ecb6676e40483c41b6e9
unknown <qp19575091@gmail.com>
2021-12-14 16:20:53 +0800

    finish

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,6 +9,16 @@
     {{-- @directive --}}
     {{-- {{ $message }} --}}
     {{-- @endDirective --}}
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     <br /><br />
     <button type="submit">Save</button>
 </form>
