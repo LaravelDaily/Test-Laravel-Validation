11274788a4e97ec1ff385ab59af807bf72640e13
yehia-khalil <yehiakhalilykk@gmail.com>
2021-12-15 13:49:36 +0200

    All tests pass

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -4,15 +4,22 @@
 {{-- in case of title/description empty, visitor should see --}}
 {{-- "The name field is required." and "The description field is required." --}}
 
+
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
     Title:
-    <br />
-    <input type="text" name="title" />
-    <br /><br />
+    <br/>
+    <input type="text" name="title"/>
+        @error('name')
+            {{$message}}
+        @enderror
+    <br/><br/>
     Description:
-    <br />
-    <input type="text" name="description" />
-    <br /><br />
+    <br/>
+    <input type="text" name="description"/>
+        @error('description')
+            {{$message}}
+        @enderror
+    <br/><br/>
     <button type="submit">Save</button>
 </form>
