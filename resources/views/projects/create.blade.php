396b7743b07c7bcdaa7a2c00cdd175479b86f912
Hina <>
2021-12-17 13:06:25 +0500

    Validation test completed

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -8,11 +8,21 @@
     @csrf
     Title:
     <br />
-    <input type="text" name="title" />
+    <input type="text" name="title" placeholder="Title" />
     <br /><br />
+				@error('title')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                @enderror
     Description:
     <br />
-    <input type="text" name="description" />
+    <input type="text" name="description" placeholder="Description" />
     <br /><br />
+				@error('description')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                @enderror
     <button type="submit">Save</button>
 </form>
