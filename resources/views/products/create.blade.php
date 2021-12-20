6dd4bee8af937b1662ae5ee5e9a9d7ec33d835fd
ahadimuhsin <muhsin_ahadi@student.uns.ac.id>
2021-12-20 09:48:16 +0700

    all passed

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,6 +3,9 @@
     Name:
     <br />
     <input type="text" name="name" />
+    @if($errors->has('name'))
+    <div class="error">{{ $errors->first('name') }}</div>
+@endif
     <br />
     {{-- TASK: show the validation error for the specific "name" field --}}
     {{-- using one Blade directive: pseudo-code below --}}
