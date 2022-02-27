50be04798593d1528c26a1eb5f6c03900a429bbf
Y2theK <yeyintkyaw300@gmail.com>
2022-02-27 15:08:36 -0800

    validation test completed again

--- app/Rules/UpperCase.php
+++ app/Rules/UpperCase.php
@@ -25,7 +25,7 @@ class UpperCase implements Rule
      */
     public function passes($attribute, $value)
     {
-        return ucfirst($value) == $value;
+        return ucfirst($value) === $value;
     }
 
     /**
@@ -35,6 +35,6 @@ class UpperCase implements Rule
      */
     public function message()
     {
-        return 'The title does not start with an uppercased letter';
+        return 'The :attribute does not start with an uppercased letter';
     }
 }
