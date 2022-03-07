0281f99f6bbcabfbffdebc9ca32e72c43fb4f283
neo <neomn110@gmail.com>
2022-03-07 14:28:06 +0330

    tasks done till task 6

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,14 +1,19 @@
 <form method="POST" action="{{ route('products.store') }}">
     @csrf
     Name:
-    <br />
-    <input type="text" name="name" />
-    <br />
+    <br/>
+    <input type="text" name="name"/>
+    <br/>
     {{-- TASK: show the validation error for the specific "name" field --}}
     {{-- using one Blade directive: pseudo-code below --}}
     {{-- @directive --}}
     {{-- {{ $message }} --}}
     {{-- @endDirective --}}
-    <br /><br />
+    @if($errors->any())
+        @error('name')
+        {{$message}}
+        @enderror
+    @endif
+    <br/><br/>
     <button type="submit">Save</button>
 </form>
