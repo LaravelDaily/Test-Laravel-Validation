80a289bc36084f33b23df77e068cdc4ac51d6549
ermimak <ermiasmekonnen28@gmail.com>
2022-03-09 15:13:36 +0300

    Done

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -17,6 +17,12 @@ class BuildingController extends Controller
     // TASK: Customize the validation error message to say "Please enter the name"
     public function store(StoreBuildingRequest $request)
     {
+        $validator = Validator::make($request->all(),$request->rules(),$request->message());
+
+        if( $validator->fails()){
+            return redirect('buildings/create')
+            ->withErrors($validator);
+        }
         Building::create($validator->validated());
 
         return 'Success';
