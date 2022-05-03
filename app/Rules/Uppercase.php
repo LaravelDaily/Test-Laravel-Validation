f027815888ee729b31f5269175cd18d1f7950db8
David Villalobos <davidvillalobosvargas@gmail.com>
2022-05-03 13:13:52 -0600

    test9v4

--- app/Rules/Uppercase.php
+++ app/Rules/Uppercase.php
@@ -25,7 +25,6 @@ class Uppercase implements Rule
      */
     public function passes($attribute, $value)
     {
-        //
         return ucfirst(strtolower($value)) == $value;
     }
 
@@ -36,6 +35,6 @@ class Uppercase implements Rule
      */
     public function message()
     {
-        return 'The title does not start with an uppercased letter.';
+        return "The title does not start with an uppercased letter";
     }
 }
