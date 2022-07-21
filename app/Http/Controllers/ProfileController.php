a6064c2d03f515fa66b1f362a6ee557f440a92c6
mhamzase <mhamzasulehri143@gmail.com>
2022-07-21 11:39:16 +0500

    all test passed. yahoooo!

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -13,6 +13,9 @@ class ProfileController extends Controller
             // <input name="profile[name]" ... />
             // <input name="profile[email]" ... />
             // Write validation rules, so both name and email are required
+            'profile' => 'array',
+            'profile.name' => 'required',
+            'profile.email' => 'required',
         ]);
 
         auth()->user()->update($request->profile ?? []);
