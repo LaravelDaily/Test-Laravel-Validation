bcabd715c3af193f5ec681b278445b6eb699dd53
suraj <surajsuwal487@gmail.com>
2022-08-02 12:06:25 +0545

    validation test successfully completed

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -4,6 +4,16 @@
 {{-- in case of title/description empty, visitor should see --}}
 {{-- "The name field is required." and "The description field is required." --}}
 
+@if ($errors->any())
+    <div class="alert alert-danger">
+        @foreach ($errors->all() as $error )
+            <ul>
+                <li>{{ $error }}</li>
+            </ul>
+        @endforeach
+    </div>
+@endif
+
 <form method="POST" action="{{ route('projects.store') }}">
     @csrf
     Title:
